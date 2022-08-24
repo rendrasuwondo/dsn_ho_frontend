3aa9172533ab5ad1b5a46d9ef7f574f2340dff87
Aida171 <aida.mahmudah171@gmail.com>
2022-08-24 11:40:51 +0700

    claim

---
+++ pages/erp_ho/fertilizer/claim/create/index.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-tasks"></i> <b>TAMBAH KLAIM HAP</b>
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="storePost">
+            <div class="form-group">
+              <label>No. Klaim</label>
+              <input
+                type="text"
+                v-model="field.claim_no"
+                placeholder="Masukkan No Klaim HAP"
+                class="form-control"
+                ref="claim_no"
+              />
+              <div v-if="validation.claim_no" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.claim_no[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Keterangan</label>
+
+              <textarea
+                v-model="field.description"
+                class="form-control"
+                rows="3"
+                placeholder="Masukkan Deskripsi Singkat"
+              ></textarea>
+              <div v-if="validation.description" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.description[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col>
+                  <label>Tanggal Buat </label>
+                  <b-form-datepicker
+                    v-model="field.created_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker>
+                </b-col>
+                <b-col
+                  ><label>Pembuat</label>
+                  <input
+                    type="text"
+                    v-model="field.created_by"
+                    class="form-control"
+                    readonly
+                /></b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col
+                  ><label>Tanggal Ubah </label>
+
+                  <b-form-datepicker
+                    v-model="field.updated_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker
+                ></b-col>
+                <b-col>
+                  <label>Pengubah</label>
+                  <input
+                    type="text"
+                    v-model="field.updated_by"
+                    class="form-control"
+                    readonly
+                  />
+                </b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group"></div>
+
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> SIMPAN
+            </button>
+            <button
+              v-on:click="back()"
+              class="btn btn-warning btn-reset"
+              type="reset"
+            >
+              <i class="fa fa-redo"></i> BATAL
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+/* import { VNumber  } from '@coders-tm/vue-number-format' */
+/* import { number } from '@coders-tm/vue-number-format' */
+
+export default {
+  //layout
+  layout: 'admin',
+
+  //meta
+  head() {
+    return {
+      title: 'Tambah Klaim HAP',
+    }
+  },
+
+  data() {
+    return {
+      state: 'disabled',
+
+      field: {
+        claim_no: '',
+        description: '',
+        created_at: '',
+        updated_at: '',
+        created_by: '',
+        updated_by: '',
+      },
+
+      //state validation
+      validation: [],
+    }
+  },
+
+  mounted() {
+    this.field.created_at = this.currentDate()
+    this.field.updated_at = this.currentDate()
+    this.field.created_by =
+      this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+    this.field.updated_by =
+      this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+
+    this.$refs.claim_no.focus()
+  },
+
+  methods: {
+    back() {
+      this.$router.push({
+        name: 'erp_ho-fertilizer-claim',
+        params: { id: this.$route.params.id, r: 1 },
+      })
+    },
+
+    currentDate() {
+      const current = new Date()
+      const date = `${current.getFullYear()}-${
+        current.getMonth() + 1
+      }-${current.getDate()}`
+
+      return date
+    },
+
+    async storePost() {
+      //define formData
+      let formData = new FormData()
+
+      formData.append('claim_no', this.field.claim_no)
+      formData.append('description', this.field.description)
+      formData.append('created_at', this.field.created_at)
+      formData.append('created_by', this.field.created_by)
+      formData.append('updated_at', this.field.update_at)
+      formData.append('udpated_by', this.field.udpate_by)
+
+      //sending data to server
+      await this.$axios
+        .post('/api/admin/claim', formData)
+        .then(() => {
+          //sweet alert
+          this.$swal.fire({
+            title: 'BERHASIL!',
+            text: 'Data Berhasil Disimpan!',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+
+          //redirect, if success store data
+          this.back()
+        })
+        .catch((error) => {
+          //assign error to state "validation"
+          this.validation = error.response.data
+        })
+    },
+  },
+
+  computed: {
+    disabled() {
+      return this.state === 'disabled'
+    },
+    readonly() {
+      return this.state === 'readonly'
+    },
+  },
+}
+</script>
+
+<style>
+.ck-editor__editable {
+  min-height: 200px;
+}
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+</style>
---
+++ pages/erp_ho/fertilizer/claim/edit/_id.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-tasks"></i> <b>EDIT KLAIM HAP</b>
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="update">
+            <div class="form-group">
+              <label>No.Klaim</label>
+              <input
+                type="text"
+                v-model="field.claim_no"
+                placeholder=""
+                class="form-control"
+                ref="claim_no"
+              />
+              <div v-if="validation.claim_no" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.claim_no[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Keterangan</label>
+              <textarea
+                v-model="field.description"
+                class="form-control"
+                rows="3"
+                placeholder=""
+              ></textarea>
+              <div v-if="validation.description" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.description[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col>
+                  <label>Tanggal Buat </label>
+                  <b-form-datepicker
+                    v-model="field.created_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker>
+                </b-col>
+                <b-col
+                  ><label>Pembuat</label>
+                  <input
+                    type="text"
+                    v-model="field.created_by"
+                    class="form-control"
+                    readonly
+                /></b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col
+                  ><label>Tanggal Ubah </label>
+
+                  <b-form-datepicker
+                    v-model="field.updated_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker
+                ></b-col>
+                <b-col>
+                  <label>Pengubah</label>
+                  <input
+                    type="text"
+                    v-model="field.updated_by"
+                    class="form-control"
+                    readonly
+                  />
+                </b-col>
+              </b-row>
+            </div>
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> SIMPAN
+            </button>
+            <button
+              v-on:click="back()"
+              class="btn btn-warning btn-reset"
+              type="reset"
+            >
+              <i class="fa fa-redo"></i> BATAL
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  //layout
+  layout: 'admin',
+
+  //meta
+  head() {
+    return {
+      title: 'Edit Klaim HAP',
+    }
+  },
+
+  data() {
+    return {
+      state: 'disabled',
+
+      field: {
+        claim_no: '',
+        description: '',
+        created_at: '',
+        updated_at: '',
+        created_by: '',
+        updated_by: '',
+      },
+
+      //state validation
+      validation: [],
+    }
+  },
+
+  mounted() {
+    //get data field by ID
+    this.$axios
+      .get(`/api/admin/claim/${this.$route.params.id}`)
+      .then((response) => {
+        //data yang diambil
+        this.field.claim_no = response.data.data.claim_no
+        this.field.description = response.data.data.description
+        this.field.created_at = response.data.data.created_at
+        this.field.created_by = response.data.data.created_by
+        this.field.updated_at = response.data.data.updated_at
+        this.field.updated_by = response.data.data.updated_by
+      })
+
+    this.$refs.claim_no.focus()
+  },
+
+  methods: {
+    back() {
+      this.$router.push({
+        name: 'erp_ho-fertilizer-claim',
+        params: { id: this.$route.params.id, r: 1 },
+      })
+    },
+
+    // update method
+    async update(e) {
+      e.preventDefault()
+
+      //send data ke Rest API untuk update
+      await this.$axios
+        .put(`/api/admin/claim/${this.$route.params.id}`, {
+          //data yang dikirim
+          claim_no: this.field.claim_no,
+          description: this.field.description,
+          created_at: this.field.created_at,
+          updated_at: this.field.updated_at,
+          created_by: this.field.created_by,
+          updated_by: this.field.updated_by,
+        })
+        .then(() => {
+          //sweet alert
+          this.$swal.fire({
+            title: 'BERHASIL!',
+            text: 'Data Berhasil Diupdate!',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+          //redirect ke route "post"
+          this.$router.push({
+            name: 'erp_ho-system-role',
+          })
+        })
+        .catch((error) => {
+          //assign error validasi
+          this.validation = error.response.data
+        })
+    },
+  },
+  computed: {
+    disabled() {
+      return this.state === 'disabled'
+    },
+    readonly() {
+      return this.state === 'readonly'
+    },
+  },
+}
+</script>
+<style>
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+</style>
