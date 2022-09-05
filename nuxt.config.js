09213d895272db510ac19bf6660ceea7bc5f2010
rendrasuwondo <rendrasuwondo@gmail.com>
2022-09-05 14:51:14 +0700

    Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -131,7 +131,7 @@ export default {
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        baseURL: 'http://localhost:8000',
+        baseURL: 'http://192.168.202.118:8004',
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
