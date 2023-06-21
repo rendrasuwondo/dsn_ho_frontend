99949cd76accccdb6ea57aca80c6632677da2734
ghalib19 <ghalib.ath@gmail.com>
2023-06-21 14:15:20 +0700

    pic

---
+++ pages/erp_ho/mdex/pic/edit/_id.vue
@@ -0,0 +1,325 @@
+<template>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <div v-if="show === 0">
+      <b-img right src="\img/dsn_logo.png" alt="" class="img-logo"></b-img>
+      <p class="txt-2">Loading</p>
+      <div class="spinonediv-4"></div>
+    </div>
+
+    <section class="content" v-if="show === 1">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-industry"></i> EDIT PIC
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="update">
+            <!-- <div class="form-group">
+              <label>NIK</label>
+              <input
+                type="text"
+                v-model="field.nik"
+                placeholder=""
+                class="form-control"
+                ref="nik"
+              />
+              <div v-if="validation.code" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.nik[0]
+                }}</b-alert>
+              </div>
+            </div> -->
+
+
+            <div class="form-group">
+              <label>Employee Id</label>
+              <input
+                type="text"
+                v-model="field.employee_id"
+                placeholder=""
+                class="form-control"
+                ref="employee_id"
+              />
+              <div v-if="validation.employee_id" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.employee_id[0]
+                }}</b-alert>
+              </div>
+              </div>
+
+            <!-- <div class="form-group">
+              <label>Nama</label>
+              <input
+                type="text"
+                v-model="field.name"
+                placeholder=""
+                class="form-control"
+              />
+              <div v-if="validation.code" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.nik[0]
+                }}</b-alert>
+              </div>
+            </div> -->
+            
+
+            <!-- <div class="form-group">
+              <label>Email</label>
+              <input
+                type="text"
+                v-model="field.email"
+                placeholder=""
+                class="form-control"
+              />
+            </div> -->
+
+            <div class="form-group">
+              <label>Aktif?</label>
+              <b-form-select v-model="field.is_active">
+                <b-form-select-option value="Y">Ya</b-form-select-option>
+                <b-form-select-option value="N">Tidak</b-form-select-option>
+              </b-form-select>
+              <div v-if="validation.is_active" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.is_active[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+
+            <div class="form-group">
+              <label>Keterangan</label>
+
+              <textarea
+                v-model="field.description"
+                class="form-control"
+                rows="3"
+                placeholder=""
+              ></textarea>
+              <!-- <div v-if="validation.description" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.description[0]
+                }}</b-alert>
+              </div> -->
+            </div>
+            <div class="form-group">
+              <b-row>
+                <b-col>
+                  <label>Tanggal Buat </label>
+                  <b-form-datepicker
+                    v-model="field.created_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker>
+                </b-col>
+                <b-col
+                  ><label>Pembuat</label>
+                  <input
+                    type="text"
+                    v-model="field.created_by"
+                    class="form-control"
+                    readonly
+                /></b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col
+                  ><label>Tanggal Ubah </label>
+
+                  <b-form-datepicker
+                    v-model="field.updated_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker
+                ></b-col>
+                <b-col>
+                  <label>Pengubah</label>
+                  <input
+                    type="text"
+                    v-model="field.updated_by"
+                    class="form-control"
+                    readonly
+                  />
+                </b-col>
+              </b-row>
+            </div>
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> SIMPAN
+            </button>
+            <button
+              v-on:click="back()"
+              class="btn btn-warning btn-reset"
+              type="reset"
+            >
+              <i class="fa fa-redo"></i> BATAL
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  //layout
+  layout: 'admin',
+
+  //meta
+  head() {
+    return {
+      title: 'Edit PIC',
+    }
+  },
+
+  data() {
+    return {
+      options: [
+        { value: 'Y', text: 'Ya' },
+        { value: 'N', text: 'Tidak' },
+      ],
+      state: 'disabled',
+      field: {
+        description: '',
+        // nik: '',
+        id: '',
+        employee_id: '',
+        // name: '',
+        // email: '',
+        is_active: '',
+        created_at: '',
+        updated_at: '',
+        created_by: '',
+        updated_by: '',
+      },
+
+      //state validation
+      validation: [],
+      show: 1,
+    }
+  },
+
+  mounted() {
+    //get data field by ID
+    this.$axios
+      .get(`/api/admin/pic/${this.$route.params.id}`)
+      .then((response) => {
+        //data yang diambil
+        // this.field.nik = response.data.data.nik
+        this.field.employee_id = response.data.data.employee_id
+        // this.field.name = response.data.data.name
+        // this.field.email = response.data.data.email
+        this.field.is_active = response.data.data.is_active
+        this.field.created_at = response.data.data.created_at
+        this.field.created_by = response.data.data.created_by
+        this.field.updated_at = response.data.data.updated_at
+        this.field.updated_by = response.data.data.updated_by
+        this.field.description = response.data.data.description
+      })
+    this.$refs.employee_id.focus()
+  },
+
+  methods: {
+    back() {
+      this.$router.push({
+        name: 'erp_ho-mdex-pic',
+        params: { id: this.$route.params.id, r: 1 },
+      })
+    },
+
+    // update method
+    async update(e) {
+      e.preventDefault()
+      this.show = 0
+
+      //send data ke Rest API untuk update
+      await this.$axios
+        .put(`/api/admin/pic/${this.$route.params.id}`, {
+          //data yang dikirim
+          // nik: this.field.nik,
+          employee_id: this.field.employee_id,
+          // name: this.field.name,
+          is_active: this.field.is_active,
+          // email: this.field.email,
+          created_at: this.field.created_at,
+          updated_at: this.field.updated_at,
+          created_by: this.field.created_by,
+          updated_by: this.field.updated_by,
+          description: this.field.description,
+        })
+        .then(() => {
+          this.show = 1
+
+          //sweet alert
+          this.$swal.fire({
+            title: 'BERHASIL!',
+            text: 'Data Berhasil Diupdate!',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+          //redirect ke route "post"
+          this.$router.push({
+            name: 'erp_ho-mdex-pic',
+          })
+        })
+        .catch((error) => {
+          this.show = 1
+
+          //assign error validasi
+          this.validation = error.response.data
+        })
+    },
+  },
+  computed: {
+    disabled() {
+      return this.state === 'disabled'
+    },
+    readonly() {
+      return this.state === 'readonly'
+    },
+  },
+}
+</script>
+<style>
+.ck-editor__editable {
+  min-height: 200px;
+}
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+.img-logo {
+  width: 160px;
+  padding-top: 10px;
+  padding-right: 20px;
+}
+.txt-2 {
+  color: #be65e2;
+  padding-top: 17%;
+  font-family: 'Press Start 2P', cursive;
+  text-align: center;
+  font-size: 27px;
+  text-shadow: 2px 2px rgba(0, 0, 0, 0.148);
+  font-weight: bold;
+}
+</style>
