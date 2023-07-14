1c114864f3882e48da245486ade101fe071b5e21
ghalib.ath <77096740+ghalib19@users.noreply.github.com>
2023-07-14 16:02:36 +0700

    inflow

---
+++ pages/erp_ho/mdex/inflow/create/_status.vue
@@ -0,0 +1,328 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <div v-if="show === 0">
+      <b-img right src="\img/dsn_logo.png" alt="" class="img-logo"></b-img>
+      <p class="txt-2">Loading</p>
+      <div class="spinonediv-4"></div>
+    </div>
+
+    <section class="content" v-if="show === 1">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-industry"></i> TAMBAH INFLOW
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="storePost">
+            <div class="form-group">
+              <label>Product</label>
+              <multiselect
+                v-model="field.product_id"
+                :options="product"
+                label="code"
+                track-by="id"
+                :searchable="true"
+              ></multiselect>
+              <div v-if="validation.product_id" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.product_id[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Inflow Date</label>
+              <b-form-datepicker
+                v-model="field.inflow_date"
+                :date-format-options="{
+                    year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }">
+                  </b-form-datepicker>
+            </div>
+            
+            <div class="form-group">
+              <label>Account Status </label>
+              <input
+                type="text"
+                v-model="field.account_status"
+                placeholder="Masukkan Account Status"
+                class="form-control"
+                ref="account_status"
+                readonly
+              />
+              <div v-if="validation.account_status" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.account_status[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Inflow </label>
+              <input
+                type="text"
+                v-model="field.inflow"
+                placeholder="Masukkan Inflow"
+                class="form-control"
+                ref="inflow"
+              />
+              <div v-if="validation.inflow" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.inflow[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Keterangan</label>
+
+              <textarea
+                v-model="field.description"
+                class="form-control"
+                rows="3"
+                placeholder="Masukkan Deskripsi Singkat"
+              ></textarea>
+              <div v-if="validation.description" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.description[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col>
+                  <label>Tanggal Buat </label>
+                  <b-form-datepicker
+                    v-model="field.created_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker>
+                </b-col>
+                <b-col
+                  ><label>Pembuat</label>
+                  <input
+                    type="text"
+                    v-model="field.created_by"
+                    class="form-control"
+                    readonly
+                /></b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col
+                  ><label>Tanggal Ubah </label>
+                  <b-form-datepicker
+                    v-model="field.updated_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker
+                ></b-col>
+                <b-col>
+                  <label>Pengubah</label>
+                  <input
+                    type="text"
+                    v-model="field.updated_by"
+                    class="form-control"
+                    readonly
+                  />
+                </b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group"></div>
+
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> SIMPAN
+            </button>
+            <button
+              v-on:click="back()"
+              class="btn btn-warning btn-reset"
+              type="reset"
+            >
+              <i class="fa fa-redo"></i> BATAL
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+/* import { VNumber  } from '@coders-tm/vue-number-format' */
+/* import { number } from '@coders-tm/vue-number-format' */
+
+export default {
+  //layout
+  layout: 'admin',
+
+  //meta
+  head() {
+    return {
+      title: 'Tambah Inflow',
+    }
+  },
+
+  data() {
+    return {
+      state: 'disabled',
+      field: {
+        product_id:     '',
+        inflow_date:    '',
+        account_status: '',
+        inflow:         '',
+        description:    '',
+        created_at:     '',
+        updated_at:     '',
+        created_by:     '',
+        updated_by:     '',
+      },
+
+      product: [],
+
+      //state validation
+      validation: [],
+      show: 1,
+    }
+  },
+
+  mounted() {
+    this.field.created_at = this.currentDate()
+    this.field.updated_at = this.currentDate()
+    this.field.created_by =
+    this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+    this.field.updated_by =
+    this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+    
+    //Data Account Status
+    this.field.account_status = this.$route.params.status
+    //Data Product
+    this.$axios
+      .get('/api/admin/lov_product')
+
+      .then((response) => {
+        // console.log(response.data.data[0])
+        this.product = response.data.data
+      })
+  },
+
+  methods: {
+    back() {
+      this.$router.push({
+        name: 'erp_ho-mdex-inflow',
+        params: { id: this.$route.params.id, r: 1 },
+      })
+    },
+
+    currentDate() {
+      const current = new Date()
+      const date = `${current.getFullYear()}-${
+        current.getMonth() + 1
+      }-${current.getDate()}`
+
+      return date
+    },
+
+    async storePost() {
+      this.show = 0
+
+      //define formData
+      let formData = new FormData()
+
+      formData.append('product_id',this.field.product_id ? this.field.product_id.id : '')
+      formData.append('inflow_date', this.field.inflow_date)
+      formData.append('account_status', this.field.account_status)
+      formData.append('inflow', this.field.inflow)
+      formData.append('description', this.field.description)
+      formData.append('created_at', this.field.created_at)
+      formData.append('created_by', this.field.created_by)
+      formData.append('updated_at', this.field.updated_at)
+      formData.append('updated_by', this.field.updated_by)
+
+      //sending data to server
+      await this.$axios
+        .post('/api/admin/inflow', formData)
+        .then(() => {
+          this.show = 1
+
+          //sweet alert
+          this.$swal.fire({
+            title: 'BERHASIL!',
+            text: 'Data Berhasil Disimpan!',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+
+          //redirect, if success store data
+          this.$router.push({
+            name: 'erp_ho-mdex-inflow',
+          })
+        })
+        .catch((error) => {
+          this.show = 1
+
+          //assign error to state "validation"
+          this.validation = error.response.data
+        })
+    },
+  },
+
+  computed: {
+    disabled() {
+      return this.state === 'disabled'
+    },
+    readonly() {
+      return this.state === 'readonly'
+    },
+  },
+}
+</script>
+
+<style>
+.ck-editor__editable {
+  min-height: 200px;
+}
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+.img-logo {
+  width: 160px;
+  padding-top: 10px;
+  padding-right: 20px;
+}
+.txt-2 {
+  color: #be65e2;
+  padding-top: 17%;
+  font-family: 'Press Start 2P', cursive;
+  text-align: center;
+  font-size: 27px;
+  text-shadow: 2px 2px rgba(0, 0, 0, 0.148);
+  font-weight: bold;
+}
+</style>
