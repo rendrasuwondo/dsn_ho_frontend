f671cf43272ca2545e4df1ecea27a5c0f9e33a69
Nicho Ferdiansyah <nicho.ferdia@gmail.com>
2023-08-30 17:02:27 +0700

    add page tbs pks mockup

--- pages/erp_ho/dashboard/tbs_pks/index.vue
+++ pages/erp_ho/dashboard/tbs_pks/index.vue
@@ -115,132 +115,145 @@
               </b-card-text>
             </b-card>
           </ClientOnly>
-          <div class="container">
+          <div class="container mb-2">
             <div class="row d-flex justify-items-center align-items-center">
               <div class="col">
-                <nuxt-link
-                  :to="{
-                    name: 'erp_ho-dashboard-tbs_pks',
-                    query: getParams(),
-                  }"
-                  class="card-ui card color-success"
-                >
+                <a href="javascript:;" class="card-ui card color-success">
                   <div class="overlay"></div>
                   <div class="card-body">
                     <h5 class="card-title">Total Plan TBS</h5>
                     <p class="card-text">41287 Jjg | 763.8 Ton</p>
                   </div>
-                </nuxt-link>
+                </a>
               </div>
 
               <div class="col">
-                <nuxt-link
-                  :to="{
-                    name: 'erp_ho-dashboard-tbs_pks',
-                    query: getParams(),
-                  }"
-                  class="card-ui card color-warning"
-                >
+                <a href="javascript:;" class="card-ui card color-warning">
                   <div class="overlay"></div>
                   <div class="card-body">
                     <h5 class="card-title">Total TBS diterima PKS</h5>
                     <p class="card-text">31280 Jjg | 578.6 Ton</p>
                   </div>
-                </nuxt-link>
+                </a>
               </div>
 
               <div class="col">
-                <nuxt-link
-                  :to="{
-                    name: 'erp_ho-dashboard-tbs_pks',
-                    query: getParams(),
-                  }"
-                  class="card-ui card color-danger"
-                >
+                <a href="javascript:;" class="card-ui card color-danger">
                   <div class="overlay"></div>
                   <div class="card-body">
                     <h5 class="card-title">Persentase Brondol</h5>
                     <p class="card-text">22.5 Ton | 2.3 %</p>
                   </div>
-                </nuxt-link>
-              </div>
-            </div>
-
-            <div class="row d-flex justify-items-center align-items-center">
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Persentase Restan</h5>
-                    <p class="card-text">15.1% | 10.3% | 3.1%</p>
-                  </div>
-                </a>
-              </div>
-
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Restan</h5>
-                    <p class="card-text">42Jjg |0.77Ton</p>
-                  </div>
                 </a>
               </div>
             </div>
-
-            <div class="row d-flex justify-items-center align-items-center">
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Akurasi RKH</h5>
-                    <p class="card-text">96.2%</p>
+          </div>
+          <div class="mb-2 ml-2 mr-2">
+            <no-ssr>
+              <b-row>
+                <b-col cols="6">
+                  <div class="chart-container-janjang">
+                    <div class="card card-outline card-info">
+                      <div class="card-body">
+                        <fusioncharts
+                          :type="chart.janjang.type"
+                          :width="chart.janjang.width"
+                          :height="chart.janjang.height"
+                          :dataformat="chart.janjang.dataFormat"
+                          :dataSource="chart.janjang.dataSource"
+                        >
+                        </fusioncharts>
+                      </div>
+                    </div>
                   </div>
-                </a>
-              </div>
-
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Avg. BJR</h5>
-                    <p class="card-text">17.3Kg</p>
+                </b-col>
+                <b-col cols="6">
+                  <div class="chart-container-tonase">
+                    <div class="card card-outline card-info">
+                      <div class="card-body">
+                        <fusioncharts
+                          :type="chart.tonase.type"
+                          :width="chart.tonase.width"
+                          :height="chart.tonase.height"
+                          :dataformat="chart.tonase.dataFormat"
+                          :dataSource="chart.tonase.dataSource"
+                        >
+                        </fusioncharts>
+                      </div>
+                    </div>
                   </div>
-                </a>
-              </div>
-            </div>
+                </b-col>
+              </b-row>
+            </no-ssr>
+          </div>
+          <div class="mb-2 ml-2 mr-2">
+            <b-row>
+              <b-col cols="6">
+                <div class="form-group">
+                  <multiselect
+                    v-model="puhus_janjang_id"
+                    :options="puhus"
+                    label="code"
+                    track-by="id"
+                    :searchable="true"
+                    placeholder="Detail Janjang"
+                    @input="changeDetailJanjang"
+                  ></multiselect>
+                </div>
+              </b-col>
 
-            <div class="row d-flex justify-items-center align-items-center">
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Ripeness</h5>
-                    <p class="card-text">79.7%</p>
+              <b-col cols="6">
+                <div class="form-group">
+                  <multiselect
+                    v-model="puhus_tonase_id"
+                    :options="puhus"
+                    label="code"
+                    track-by="id"
+                    :searchable="true"
+                    placeholder="Detail Tonase"
+                    @input="changeDetailTonase"
+                  ></multiselect>
+                </div>
+              </b-col>
+            </b-row>
+          </div>
+          <div class="mb-2 ml-2 mr-2">
+            <no-ssr>
+              <b-row>
+                <b-col cols="6">
+                  <div class="chart-container-detail-janjang">
+                    <div class="card card-outline card-info">
+                      <div class="card-body">
+                        <fusioncharts
+                          :type="chart.detail_janjang.type"
+                          :width="chart.detail_janjang.width"
+                          :height="chart.detail_janjang.height"
+                          :dataformat="chart.detail_janjang.dataFormat"
+                          :dataSource="chart.detail_janjang.dataSource"
+                        >
+                        </fusioncharts>
+                      </div>
+                    </div>
                   </div>
-                </a>
-              </div>
-
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Produktivitas SKU</h5>
-                    <p class="card-text">1.1 Ton/day</p>
+                </b-col>
+                <b-col cols="6">
+                  <div class="chart-container-detail-tonase">
+                    <div class="card card-outline card-info">
+                      <div class="card-body">
+                        <fusioncharts
+                          :type="chart.detail_tonase.type"
+                          :width="chart.detail_tonase.width"
+                          :height="chart.detail_tonase.height"
+                          :dataformat="chart.detail_tonase.dataFormat"
+                          :dataSource="chart.detail_tonase.dataSource"
+                        >
+                        </fusioncharts>
+                      </div>
+                    </div>
                   </div>
-                </a>
-              </div>
-
-              <div class="col">
-                <a href="javascript:;" class="card-ui card color-success">
-                  <div class="overlay"></div>
-                  <div class="card-body">
-                    <h5 class="card-title">Performance Unit Angkut</h5>
-                    <p class="card-text">67,6%</p>
-                  </div>
-                </a>
-              </div>
-            </div>
+                </b-col>
+              </b-row>
+            </no-ssr>
           </div>
         </div>
       </div>
@@ -249,6 +262,209 @@
 </template>
 
 <script>
+let chartData = {
+  caption: 'RKH vs Unplanned Transaction vs Aktual Panen',
+  theme: 'fusion',
+  xaxisname: '',
+  yaxisname: '',
+  formatnumberscale: '1',
+  // plottooltext:
+  //   '<b>$dataValue</b> <b>$seriesName</b> in $label',
+  drawcrossline: '1',
+}
+async function getChartJanjangDataSource() {
+  return {
+    chart: chartData,
+    categories: [
+      {
+        category: [
+          {
+            label: 'Puhus 1',
+          },
+          {
+            label: 'Puhus 2',
+          },
+          {
+            label: 'Puhus 3',
+          },
+        ],
+      },
+    ],
+    dataset: [
+      {
+        seriesname: 'RKH',
+        data: [
+          {
+            value: '125000',
+          },
+          {
+            value: '300000',
+          },
+          {
+            value: '480000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Total Unplanned diterima PKS',
+        data: [
+          {
+            value: '70000',
+          },
+          {
+            value: '150000',
+          },
+          {
+            value: '350000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Diterima PKS',
+        data: [
+          {
+            value: '10000',
+          },
+          {
+            value: '100000',
+          },
+          {
+            value: '300000',
+          },
+        ],
+      },
+    ],
+  }
+}
+
+async function getChartDetailJanjangDataSource() {
+  return {
+    chart: chartData,
+    categories: [
+      {
+        category: [
+          {
+            label: 'Afdeling 1',
+          },
+          {
+            label: 'Afdeling 2',
+          },
+          {
+            label: 'Afdeling 3',
+          },
+        ],
+      },
+    ],
+    dataset: [
+      {
+        seriesname: 'RKH',
+        data: [
+          {
+            value: '125000',
+          },
+          {
+            value: '300000',
+          },
+          {
+            value: '480000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Total Unplanned diterima PKS',
+        data: [
+          {
+            value: '70000',
+          },
+          {
+            value: '150000',
+          },
+          {
+            value: '350000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Diterima PKS',
+        data: [
+          {
+            value: '10000',
+          },
+          {
+            value: '100000',
+          },
+          {
+            value: '300000',
+          },
+        ],
+      },
+    ],
+  }
+}
+async function getChartDetailJanjangDataSource2() {
+  return {
+    chart: chartData,
+    categories: [
+      {
+        category: [
+          {
+            label: 'Afdeling 4',
+          },
+          {
+            label: 'Afdeling 5',
+          },
+          {
+            label: 'Afdeling 6',
+          },
+        ],
+      },
+    ],
+    dataset: [
+      {
+        seriesname: 'RKH',
+        data: [
+          {
+            value: '70000',
+          },
+          {
+            value: '430000',
+          },
+          {
+            value: '120000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Total Unplanned diterima PKS',
+        data: [
+          {
+            value: '400000',
+          },
+          {
+            value: '230000',
+          },
+          {
+            value: '230000',
+          },
+        ],
+      },
+      {
+        seriesname: 'Diterima PKS',
+        data: [
+          {
+            value: '30000',
+          },
+          {
+            value: '500000',
+          },
+          {
+            value: '800000',
+          },
+        ],
+      },
+    ],
+  }
+}
 export default {
   layout: 'admin',
 
@@ -262,9 +478,25 @@ export default {
       afdeling: [],
       department: [],
       company: [],
+      puhus: [
+        {
+          id: 1,
+          code: 'Puhus 1',
+        },
+        {
+          id: 2,
+          code: 'Puhus 2',
+        },
+        {
+          id: 3,
+          code: 'Puhus 3',
+        },
+      ],
       afdeling_id: this.$route.query.q_afdeling_id,
       department_id: this.$route.query.q_department_id,
       company_id: this.$route.query.q_company_id,
+      puhus_janjang_id: null,
+      puhus_tonase_id: null,
     }
   },
   watchQuery: [
@@ -324,6 +556,40 @@ export default {
     // )
 
     return {
+      chart: {
+        tonase: {
+          type: 'mscolumn2d',
+          renderAt: 'chart-container-janjang',
+          width: '100%',
+          height: '350',
+          dataFormat: 'json',
+          dataSource: await getChartJanjangDataSource(),
+        },
+        janjang: {
+          type: 'mscolumn2d',
+          renderAt: 'chart-container-tonase',
+          width: '100%',
+          height: '350',
+          dataFormat: 'json',
+          dataSource: await getChartJanjangDataSource(),
+        },
+        detail_tonase: {
+          type: 'mscolumn2d',
+          renderAt: 'chart-container-detail-janjang',
+          width: '100%',
+          height: '350',
+          dataFormat: 'json',
+          dataSource: await getChartDetailJanjangDataSource(),
+        },
+        detail_janjang: {
+          type: 'mscolumn2d',
+          renderAt: 'chart-container-detail-tonase',
+          width: '100%',
+          height: '350',
+          dataFormat: 'json',
+          dataSource: await getChartDetailJanjangDataSource(),
+        },
+      },
       // posts: posts.data,
       activitied_at_start: activitied_at_start,
       activitied_at_end: activitied_at_end,
@@ -336,14 +602,13 @@ export default {
   },
 
   methods: {
-    getParams() {
-      return {
-        activitied_at_prepend: 'hehehe',
-        activitied_at_append: '',
-        q_afdeling_id: '',
-        q_department_id: '',
-        q_company_id: '',
-      }
+    async changeDetailJanjang(selected) {
+      this.chart.detail_janjang.dataSource =
+        await getChartDetailJanjangDataSource2()
+    },
+    async changeDetailTonase(selected) {
+      this.chart.detail_tonase.dataSource =
+        await getChartDetailJanjangDataSource2()
     },
     changePage(page) {
       this.$router.push({
