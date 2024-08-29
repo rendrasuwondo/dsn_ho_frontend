b79c17c3aa4e36cd1fad5b006ac2fde50e027b1b
aldrinnurilyas12 <aldrinnoor1201@gmail.com>
2024-08-29 08:43:05 +0700

    update people map department & map location

---
+++ pages/erp_ho/people/map_department/create/index.vue
@@ -0,0 +1,328 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <div v-if="show === 0">
+      <b-img right src="\img/dsn_logo.png" alt="" class="img-logo"></b-img>
+      <p class="txt-2">Loading</p>
+      <div class="spinonediv-4"></div>
+    </div>
+
+    <section class="content" v-if="show === 1">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-cog"></i> <b>Tambah Map Department People Hub</b>
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="storePost">
+
+            <div class="form-group">
+              <label>SBU</label>
+              <input
+                type="text"
+                v-model="field.sbu"
+                placeholder="Masukkan SBU"
+                class="form-control"
+                ref="code"
+              />
+              <div v-if="validation.sbu" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.sbu[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Kode Organization</label>
+              <input
+                type="text"
+                v-model="field.organization_code_peoplehub"
+                placeholder="Masukkan kode organization People Hub"
+                class="form-control"
+                ref="code"
+              />
+              <div v-if="validation.organization_code_peoplehub" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.organization_code_peoplehub[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Nama Organization</label>
+              <input
+                type="text"
+                v-model="field.organization_name_peoplehub"
+                placeholder="Masukan Nama Organization PeopleHub"
+                class="form-control"
+              />
+              <div v-if="validation.organization_name_peoplehub" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.organization_name_peoplehub[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+
+            <div class="form-group">
+              <label>Kode Department</label>
+              <input
+                type="text"
+                v-model="field.department_code"
+                placeholder="Masukan Nama Kode Department"
+                class="form-control"
+              />
+              <div v-if="validation.department_code" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.department_code[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Aktif?</label>
+              <b-form-select v-model="field.is_active">
+                <b-form-select-option value="Y">Ya</b-form-select-option>
+                <b-form-select-option value="N">Tidak</b-form-select-option>
+              </b-form-select>
+            </div>
+
+            <div class="form-group">
+              <label>Keterangan</label>
+              <textarea
+                v-model="field.description"
+                class="form-control"
+                rows="3"
+                placeholder="Masukkan Deskripsi Singkat"
+              ></textarea>
+            </div>
+
+            
+            <div class="form-group">
+              <b-row>
+                <b-col>
+                  <label>Tanggal Buat </label>
+                  <b-form-datepicker
+                    v-model="field.created_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker>
+                </b-col>
+                <b-col
+                  ><label>Pembuat</label>
+                  <input
+                    type="text"
+                    v-model="field.created_by"
+                    class="form-control"
+                    readonly
+                /></b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group">
+              <b-row>
+                <b-col
+                  ><label>Tanggal Ubah </label>
+
+                  <b-form-datepicker
+                    v-model="field.updated_at"
+                    :date-format-options="{
+                      year: 'numeric',
+                      month: 'short',
+                      day: '2-digit',
+                      weekday: 'short',
+                    }"
+                    :disabled="disabled"
+                  ></b-form-datepicker
+                ></b-col>
+                <b-col>
+                  <label>Pengubah</label>
+                  <input
+                    type="text"
+                    v-model="field.updated_by"
+                    class="form-control"
+                    readonly
+                  />
+                </b-col>
+              </b-row>
+            </div>
+
+            <div class="form-group"></div>
+
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> SIMPAN
+            </button>
+            <button
+              v-on:click="back()"
+              class="btn btn-warning btn-reset"
+              type="reset"
+            >
+              <i class="fa fa-redo"></i> BATAL
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  //layout
+  layout: 'admin',
+
+  //meta
+  head() {
+    return {
+      title: 'Tambah Map Lokasi People Hub',
+    }
+  },
+
+  data() {
+    return {
+      is_active: { value: 'Y', text: 'Ya' },
+      options: [
+        { value: 'Y', text: 'Ya' },
+        { value: 'N', text: 'Tidak' },
+      ],
+
+      state: 'disabled',
+
+      field: {
+        sbu: '', 
+        organization_code_peoplehub: '',
+        organization_name_peoplehub: '',
+        department_code: '',
+        is_active: 'Y',
+        description:'',
+        created_at: '',
+        created_by: '',
+        updated_at: '',
+        updated_by: '',
+      },
+
+      //state validation
+      validation: [],
+      show: 1,
+    }
+  },
+
+  mounted() {
+    this.field.created_at = this.currentDate()
+    this.field.updated_at = this.currentDate()
+    this.field.created_by =
+      this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+    this.field.updated_by =
+      this.$auth.user.employee.nik + '-' + this.$auth.user.employee.name
+
+    this.$refs.code.focus()
+  },
+
+  methods: {
+    back() {
+      this.$router.push({
+        name: 'erp_ho-people-map_location_peoplehub',
+        params: { id: this.$route.params.id, r: 1 },
+      })
+    },
+
+    currentDate() {
+      const current = new Date()
+      const date = `${current.getFullYear()}-${
+        current.getMonth() + 1
+      }-${current.getDate()}`
+
+      return date
+    },
+
+    async storePost() {
+      this.show = 0
+
+      //define formData
+      let formData = new FormData()
+      formData.append('sbu', this.field.sbu)
+      formData.append('organization_code_peoplehub', this.field.organization_code_peoplehub)
+      formData.append('organization_name_peoplehub', this.field.organization_name_peoplehub)
+      formData.append('department_code', this.field.department_code)
+      formData.append('is_active', this.field.is_active)
+      formData.append('description', this.field.description)
+      formData.append('created_at', this.field.created_at)
+      formData.append('created_by', this.field.created_by)
+      formData.append('updated_at', this.field.updated_at)
+      formData.append('udpated_by', this.field.updated_by)
+
+      //sending data to server
+      await this.$axios
+        .post('/api/admin/map_department_people', formData)
+        .then(() => {
+          this.show = 1
+
+          //sweet alert
+          this.$swal.fire({
+            title: 'BERHASIL!',
+            text: 'Data Berhasil Disimpan!',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+
+          //redirect, if success store data
+          this.$router.push({
+            name: 'erp_ho-people-map_department',
+          })
+        })
+        .catch((error) => {
+          this.show = 1
+
+          //assign error to state "validation"
+          this.validation = error.response.data
+        })
+    },
+  },
+
+  computed: {
+    disabled() {
+      return this.state === 'disabled'
+    },
+    readonly() {
+      return this.state === 'readonly'
+    },
+  },
+}
+</script>
+
+<style>
+.ck-editor__editable {
+  min-height: 200px;
+}
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+.img-logo {
+  width: 160px;
+  padding-top: 10px;
+  padding-right: 20px;
+}
+.txt-2 {
+  color: #be65e2;
+  padding-top: 17%;
+  font-family: 'Press Start 2P', cursive;
+  text-align: center;
+  font-size: 27px;
+  text-shadow: 2px 2px rgba(0, 0, 0, 0.148);
+  font-weight: bold;
+}
+</style>
---
+++ pages/erp_ho/people/map_location_people/index.vue
@@ -0,0 +1,279 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-cog"></i> <b>Data Map Lokasi People Hub</b>
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <nuxt-link
+                  :to="{ name: 'erp_ho-people-map_location_people-create' }"
+                  class="btn btn-info btn-sm"
+                  style="padding-top: 8px"
+                  title="Tambah"
+                  ><i class="fa fa-plus-circle"></i>
+                </nuxt-link>
+                <button
+                  title="Export To Excel"
+                  class="btn btn-info"
+                  @click="exportData"
+                >
+                  <i class="fa fa-file-excel"></i>
+                </button>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                v-model="search"
+                @keypress.enter="searchData"
+                placeholder="Cari data..."
+              />
+              <div class="input-group-append">
+                <button @click="searchData" class="btn btn-info">
+                  <i class="fa fa-search"></i>
+                  CARI
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- table -->
+          <b-table
+            small
+            responsive
+            striped
+            bordered
+            hover
+            :items="posts"
+            :fields="fields"
+            show-empty
+          >
+            <template v-slot:cell(actions)="row">
+              <b-button
+                :to="{
+                  name: 'erp_ho-people-map_location_people-edit-id',
+                  params: { id: row.item.id },
+                }"
+                variant="link"
+                size="sm"
+                title="Edit"
+              >
+                <i class="fa fa-pencil-alt"></i>
+              </b-button>
+              <b-button
+                variant="link"
+                size="sm"
+                @click="deleteRole(row.item.id)"
+                title="Hapus"
+                ><i class="fa fa-trash"></i
+              ></b-button>
+            </template>
+          </b-table>
+          <!-- pagination -->
+          <b-row>
+            <b-col
+              ><b-pagination
+                v-model="pagination.current_page"
+                :total-rows="pagination.total"
+                :per-page="pagination.per_page"
+                @change="changePage"
+                align="left"
+                class="mt-1"
+              ></b-pagination
+            ></b-col>
+            <b-col class="text-right" align-self="center"
+              >{{ rowcount }} data</b-col
+            >
+          </b-row>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  head() {
+    return {
+      title: 'Map Location People Hub',
+    }
+  },
+  
+  
+    data() {
+      return {
+        //header table  
+        fields: [
+        {
+          label: 'Actions',
+          key: 'actions',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Location Code',
+          key: 'location_code_peoplehub',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Location Name',
+          key: 'location_name_peoplehub',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Location Code',
+          key: 'location_code',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Aktif',
+          key: 'is_active_code',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+      ],
+        //posts data
+        posts: [],
+      }
+    },
+
+   
+  watchQuery: ['q', 'page'],
+
+  async asyncData({ $axios, query }) {
+    //page
+    let page = query.page ? parseInt(query.page) : ''
+
+    //search
+    let search = query.q ? query.q : ''
+
+    //fetching posts
+    const posts = await $axios.$get(
+      `/api/admin/map_location_people?q=${search}&page=${page}`
+    )
+    console.log('posts',posts)
+
+    return {
+      posts: posts.data.data,
+      pagination: posts.data,
+      search: search,
+      rowcount: posts.data.total,
+    }
+  },
+
+  methods: {
+    changePage(page) {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.$route.query.q,
+          page: page,
+        },
+      })
+    },
+    //searchData
+    searchData() {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.search,
+        },
+      })
+    },
+
+    //deletePost method
+    deleteRole(id) {
+      this.$swal
+        .fire({
+          title: 'APAKAH ANDA YAKIN ?',
+          text: 'INGIN MENGHAPUS DATA INI !',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#d33',
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: 'YA, HAPUS!',
+          cancelButtonText: 'TIDAK',
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            //delete tag from server
+
+            this.$axios.delete(`/api/admin/map_location_people/${id}`)
+              .then((response) => {
+                //feresh data
+                 this.$nuxt.refresh()
+                if (response.data.success == true) {
+                  this.$swal.fire({
+                  title: 'BERHASIL!',
+                  text: 'Data Berhasil Dihapus!',
+                  icon: 'success',
+                  showConfirmButton: false,
+                  timer: 2000,
+                })
+                  
+                } else {
+                  this.$swal.fire({
+                  title: 'GAGAL!',
+                  text: 'Data Gagal dihapus!',
+                  icon: 'error',
+                  showConfirmButton: false,
+                  timer: 2000,
+                })
+                }
+
+                
+                //alert
+                this.$swal.fire({
+                  title: this.sweet_alert.title,
+                  text: response.data.message,
+                  icon: this.sweet_alert.icon,
+                  showConfirmButton: false,
+                  timer: 2000,
+                })
+              })
+          }
+        })
+    },
+
+   exportData() {
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+
+      this.$axios({
+        url: `/api/admin/map_location_people-export?q=${this.search}`,
+        method: 'GET',
+        responseType: 'blob',
+        headers: headers, // important
+      }).then((response) => {
+        this.isLoading = false
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        var fileName = 'Map_Location_PeopleHub.xlsx'
+        link.setAttribute('download', fileName) //or any other extension
+        document.body.appendChild(link)
+        link.click()
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+</style>
