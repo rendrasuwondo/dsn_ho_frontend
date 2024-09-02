bc96c016c392b11972f4b901a33047c9218d55ac
aldrinnurilyas12 <aldrinnoor1201@gmail.com>
2024-09-02 08:54:20 +0700

    set local to server

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,8 +135,8 @@ export default {
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        // baseURL: 'http://192.168.202.118:8004',
-         baseURL: 'http://localhost:8000'
+         baseURL: 'http://192.168.202.118:8004',
+        //  baseURL: 'http://localhost:8000'
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
