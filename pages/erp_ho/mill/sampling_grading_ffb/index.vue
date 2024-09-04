7b8de032d0b6310447b7fd3b8fdafb41858c616b
aldrinnurilyas12 <aldrinnoor1201@gmail.com>
2024-09-04 08:46:45 +0700

    sampling_grading

---
+++ pages/erp_ho/mill/sampling_grading_ffb/index.vue
@@ -0,0 +1,319 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-cog"></i> <b>Data Sampling Grading Ffb</b>
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <nuxt-link
+                  :to="{ name: 'erp_ho-mill-mill_type-create' }"
+                  class="btn btn-info btn-sm"
+                  style="padding-top: 8px"
+                  title="Tambah"
+                  ><i class="fa fa-plus-circle"></i>
+                </nuxt-link>
+                <button
+                  title="Export To Excel"
+                  class="btn btn-info"
+                  @click="exportData"
+                >
+                  <i class="fa fa-file-excel"></i>
+                </button>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                v-model="search"
+                @keypress.enter="searchData"
+                placeholder="Cari data..."
+              />
+              <div class="input-group-append">
+                <button @click="searchData" class="btn btn-info">
+                  <i class="fa fa-search"></i>
+                  CARI
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- table -->
+          <b-table
+            small
+            responsive
+            striped
+            bordered
+            hover
+            :items="posts"
+            :fields="fields"
+            show-empty
+          >
+            <!-- <template v-slot:cell(actions)="row">
+              <b-button
+                :to="{
+                  name: 'erp_ho-mill-mill_type-edit-id',
+                  params: { id: row.item.id },
+                }"
+                variant="link"
+                size="sm"
+                title="Edit"
+              >
+                <i class="fa fa-pencil-alt"></i>
+              </b-button>
+              <b-button
+                variant="link"
+                size="sm"
+                @click="deleteRole(row.item.id)"
+                title="Hapus"
+                ><i class="fa fa-trash"></i
+              ></b-button>
+            
+            </template> -->
+          </b-table>
+          <!-- pagination -->
+          <b-row>
+            <b-col
+              ><b-pagination
+                v-model="pagination.current_page"
+                :total-rows="pagination.total"
+                :per-page="pagination.per_page"
+                @change="changePage"
+                align="left"
+                class="mt-1"
+              ></b-pagination
+            ></b-col>
+            <b-col class="text-right" align-self="center"
+              >{{ rowcount }} data</b-col
+            >
+          </b-row>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  head() {
+    return {
+      title: 'Sampling Grading Ffb',
+    }
+  },
+  
+  
+    data() {
+      return {
+        //header table  
+        fields: [
+        {
+          label: 'No',
+          key: 'No',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Description',
+          key: 'Description',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Unit',
+          key: 'Unit',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 1',
+          key: 'Afdeling1',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 2',
+          key: 'Afdeling2',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 3',
+          key: 'Afdeling2',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 4',
+          key: 'Afdeling4',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 5',
+          key: 'Afdeling5',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 6',
+          key: 'Afdeling6',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 7',
+          key: 'Afdeling7',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 7',
+          key: 'Afdeling7',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 8',
+          key: 'Afdeling8',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 9',
+          key: 'Afdeling9',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Afdeling 10',
+          key: 'Afdeling10',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Total',
+          key: 'total',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        }
+      ],
+        //posts data
+        posts: [],
+      }
+    },
+
+   
+  watchQuery: ['q', 'page'],
+
+  async asyncData({ $axios, query }) {
+    //page
+    let page = query.page ? parseInt(query.page) : ''
+
+    //search
+    let search = query.q ? query.q : ''
+
+    //fetching posts
+    const posts = await $axios.$get(
+      `/api/agro-dashboard/sampling_grading_ffb?q=${search}&page=${page}`
+    )
+    console.log('posts',posts)
+
+    return {
+      posts: posts.data.data,
+      pagination: posts.data,
+      search: search,
+      rowcount: posts.data.total,
+    }
+  },
+
+  methods: {
+    changePage(page) {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.$route.query.q,
+          page: page,
+        },
+      })
+    },
+    //searchData
+    searchData() {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.search,
+        },
+      })
+    },
+
+    //deletePost method
+    deleteRole(id) {
+      this.$swal
+        .fire({
+          title: 'APAKAH ANDA YAKIN ?',
+          text: 'INGIN MENGHAPUS DATA INI !',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#d33',
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: 'YA, HAPUS!',
+          cancelButtonText: 'TIDAK',
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            //delete tag from server
+
+            this.$axios.delete(`/api/admin/milltype/${id}`)
+              .then((response) => {
+                //feresh data
+                 this.$nuxt.refresh()
+                if (response.data.success == true) {
+                  this.sweet_alert.title = 'BERHASIL!'
+                  this.sweet_alert.icon = 'success'
+                  
+                } else {
+                  this.sweet_alert.title = 'GAGAL!'
+                  this.sweet_alert.icon = 'error'
+                }
+
+                //alert
+                this.$swal.fire({
+                  title: this.sweet_alert.title,
+                  text: response.data.message,
+                  icon: this.sweet_alert.icon,
+                  showConfirmButton: false,
+                  timer: 2000,
+                })
+              })
+          }
+        })
+    },
+
+   exportData() {
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+
+      this.$axios({
+        url: `/api/admin/milltype-export?q=${this.search}`,
+        method: 'GET',
+        responseType: 'blob',
+        headers: headers, // important
+      }).then((response) => {
+        this.isLoading = false
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        var fileName = 'Milltype.xlsx'
+        link.setAttribute('download', fileName) //or any other extension
+        document.body.appendChild(link)
+        link.click()
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card-info.card-outline {
+  border-top: 5px solid #504d8d;
+}
+.card-title {
+  color: #504d8d;
+}
+</style>
